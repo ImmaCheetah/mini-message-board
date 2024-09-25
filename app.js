0a28d64cb0646c9b2702a48a8db8ca5abadb5668
cheet <29075792+ImmaCheetah@users.noreply.github.com>
2024-09-25 14:52:25 -0400

    style: add styling to main page

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const indexRouter = require('./routes/indexRouter')
 
 app.set("views", path.join(__dirname, "views/pages"));
 app.set("view engine", "ejs");
+const assetsPath = path.join(__dirname, "/public");
+app.use(express.static(assetsPath));
+
 
 app.use(express.urlencoded({ extended: true }));
 
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,38 +3,36 @@ const { v4: uuidv4 } = require('uuid');
 const indexRouter = Router();
 
 const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-const today  = new Date();
 
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: today.toLocaleString("en-US", options),
+    added: new Date().toLocaleString("en-US", options),
     id: uuidv4()
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: today.toLocaleString("en-US", options),
+    added: new Date().toLocaleString("en-US", options),
     id: uuidv4()
   }
 ];
 
 indexRouter.get('/', (req, res) => {
-  res.render('index', {messages: messages})
+  res.render('index', {messages: messages, title: 'Message Board'})
 })
 
 indexRouter.get('/new', (req, res) => {
-  res.render('form')
+  res.render('form', {title: 'New Message'})
 })
 
 indexRouter.get('/message/:id', (req, res) => {
-  console.log("Params:", req.params);
-  res.render('message', {messages: messages, param: req.params})
+  res.render('message', {messages: messages, param: req.params, title: 'Message'})
 })
 
 indexRouter.post('/new', (req, res) => {
-  messages.push({ text: req.body.message, user: req.body.name, added: today.toLocaleString("en-US", options), id: uuidv4() });
+  messages.push({ text: req.body.message, user: req.body.name, added: new Date().toLocaleString("en-US", options), id: uuidv4() });
 
   res.redirect('/')
 })
