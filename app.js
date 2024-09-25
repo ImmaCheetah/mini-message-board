e69a935cdc85d41e7895a7f90f7b34bfb8fed0d7
cheet <29075792+ImmaCheetah@users.noreply.github.com>
2024-09-25 10:15:36 -0400

    feat: add open button that opens message based on id

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const path = require("node:path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const indexRouter = require('./routes/indexRouter')
 
 app.set("views", path.join(__dirname, "views/pages"));
 app.set("view engine", "ejs");
 
-const indexRouter = require('./routes/indexRouter')
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter)
 app.use('/new', indexRouter)
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,15 +6,16 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date(),
+    id: 1
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
+    added: new Date(),
+    id: 2
   }
 ];
-
 
 indexRouter.get('/', (req, res) => {
   res.render('index', {messages: messages})
@@ -24,8 +25,14 @@ indexRouter.get('/new', (req, res) => {
   res.render('form')
 })
 
+indexRouter.get('/message/:id', (req, res) => {
+  res.render('message')
+})
+
 indexRouter.post('/new', (req, res) => {
-  res.send('message posted')
+  messages.push({ text: req.body.message, user: req.body.name, added: new Date() });
+
+  res.redirect('/')
 })
 
 module.exports = indexRouter;
