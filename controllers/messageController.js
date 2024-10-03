763aeb1951aabcc1250211f2cc610efe5e08e254
cheet <29075792+ImmaCheetah@users.noreply.github.com>
2024-10-03 16:15:14 -0400

    feat: add opening message functionality with db

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,28 +1,11 @@
-const { v4: uuidv4 } = require('uuid');
 const db = require("../db/queries");
-const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-
-// const messages = [
-//     {
-//       text: "I am looking into the void",
-//       user: "Sava",
-//       added: new Date().toLocaleString("en-US", options),
-//       id: uuidv4()
-//     },
-//     {
-//       text: "Hello World!",
-//       user: "Charles",
-//       added: new Date().toLocaleString("en-US", options),
-//       id: uuidv4()
-//     }
-// ];
 
 async function getIndex(req, res) {
     try {
         const messages = await db.getAllMessages();
         res.render('index', {messages: messages, title: 'Message Board'})
     } catch (error) {
-        console.error("Error retrieving user:", error);
+        console.error(error);
         res.status(500).send("Internal Server Error");
     }
 }
@@ -31,22 +14,22 @@ function getForm(req, res) {
     res.render('form', {title: 'New Message'})
 }
 
-function getMessage(req, res) {
-    res.render('message', {messages: messages, param: req.params, title: 'Message'})
+async function getMessage(req, res) {
+    try {
+        const messages = await db.getAllMessages()
+        res.render('message', {messages: messages, param: req.params, title: 'Message'})
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 async function createMessage(req, res) {
-    // messages.push({ text: req.body.message, user: req.body.name, added: new Date().toLocaleString("en-US", options), id: uuidv4() });
-    const {name, message} = req.body;
-    // console.log(username, text)
-    await db.insertMessage(name, message)
-    console.log(req.body)
-
-    // try {
-    //     const message = 
-    // } catch (error) {
-        
-    // }
+    try {
+        const {name, message} = req.body;
+        await db.insertMessage(name, message)
+    } catch (error) {
+        console.log(error)
+    }
 
     res.redirect('/')
 }
