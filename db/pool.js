45fda5c167cab7f81aeacc7ee72a778e33a98951
cheet <29075792+ImmaCheetah@users.noreply.github.com>
2024-10-04 10:59:32 -0400

    fix: change pool to koyeb db

--- db/pool.js
+++ db/pool.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-module.exports = new Pool({
-  host: process.env.HOST, 
-  user: process.env.USER,
-  database: process.env.DB,
-  password: process.env.PASSWORD,
-  port: process.env.POOL_PORT // The default port
-});
-
 // module.exports = new Pool({
-//   connectionString: `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`
+//   host: process.env.HOST, 
+//   user: process.env.USER,
+//   database: process.env.DB,
+//   password: process.env.PASSWORD,
+//   port: process.env.POOL_PORT // The default port
 // });
+
+module.exports = new Pool({
+  connectionString: `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`
+});
